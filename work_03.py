c7b80585b154928d39fe6790156809a29df89175
tiputa <yoshinagaaren0510@gmail.com>
2025-07-03 11:40:20 +0900

    a

--- work_03.py
+++ work_03.py
@@ -10,14 +10,15 @@ dict = {
     "沖縄県":"黒糖"
 }
 
-a = random.choice(list(dict.keys()))
+ken = random.choice(list(dict.keys()))
 
 print("これから出す都道府県の名産品（食べ物）を答えよ")
+answer = input(f"{ken}の名産品は？：")
 
-
-a = input(a)
-
-if a == input():
+if answer == dict[ken]:
     print("正解")
 else:
     print("不正解")
+print(f"{ken}の名産品は{dict[ken]}です")
+print("お疲れ様でした")
+print("次の問題に行きます")
